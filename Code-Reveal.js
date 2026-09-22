7fc12e1c9d143ff291f8b886de8c95ea45067282
C0letral <102485158+C0letral@users.noreply.github.com>
2026-09-22 17:13:34 +0200

    reveal code added
    added reveal code made with the help of Gemini, planning to tinker and see what i can do after a bunch more coding

---
+++ Code-Reveal.js
@@ -0,0 +1,15 @@
+const clickArea = document.getElementById('clickable-area');
+const textcontainer = document.getElementById('text-container');
+
+clickArea.addEventListener('click', function() {
+
+if (textcontainer.innerHTML === "") {
+    const newText = document.createElement('p');
+
+    newText.textContent = "Code";
+    newText.className = "spawned-text";
+
+    textContainer.appendChild(newText);
+}    
+
+});
